accb96a5d2d3b84d74f2c96956638a347964bcf0
omicreate <omi0612.test@gmail.com>
2026-05-27 17:21:00 +0900

    Use white text for active unknown player

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soft-tennis-logger-v114";
+const CACHE_NAME = "soft-tennis-logger-v115";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
