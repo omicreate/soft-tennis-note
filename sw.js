dc6e500cbc112d55665e9da8a662e6e16db0a0d9
omicreate <omi0612.test@gmail.com>
2026-05-27 17:18:17 +0900

    Arrange player buttons in one row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soft-tennis-logger-v113";
+const CACHE_NAME = "soft-tennis-logger-v114";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
