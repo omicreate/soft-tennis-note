ed9ca99f471e0aed259766531c1a447d0491e2c4
omicreate <omi0612.test@gmail.com>
2026-05-27 16:43:47 +0900

    Hide CSV export under admin menu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soft-tennis-logger-v108";
+const CACHE_NAME = "soft-tennis-logger-v109";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
