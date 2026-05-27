09ccb1f57223d5984a481df59eadea54611d38b6
omicreate <omi0612.test@gmail.com>
2026-05-27 17:08:18 +0900

    Improve mobile player control layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "soft-tennis-logger-v109";
+const CACHE_NAME = "soft-tennis-logger-v111";
 const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
